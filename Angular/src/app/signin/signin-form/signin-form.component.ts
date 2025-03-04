e7cb6b37262994c891e090833ceaad31ba00bbf7
Yasseen Ahmed <yasseenlmaghraby3@gmail.com>
2025-03-04 06:58:09 +0200

    added verfying page

---
+++ Angular/src/app/signin/signin-form/signin-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signin-form',
+  imports: [],
+  templateUrl: './signin-form.component.html',
+  styleUrl: './signin-form.component.scss'
+})
+export class SigninFormComponent {
+
+}
