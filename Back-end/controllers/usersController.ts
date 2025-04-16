a7696806981b213aa65a306113c749d047af6c75
Baher Hossam <Baher1088@gmail.com>
2025-04-16 15:24:40 +0200

    backend update

--- Back-end/controllers/usersController.ts
+++ Back-end/controllers/usersController.ts
@@ -1,42 +1,42 @@
 import { Request, Response, NextFunction } from "express";
 import usersModel from "../Models/usersModel";
-import { users } from "../Interfaces/usersInterface";
+import {users}  from "../Interfaces/usersInterface";
 import asyncHandler from "express-async-handler";
 import { deleteFingerprint } from "./FingerprintController";
 
-export const createUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+export const createUser = asyncHandler(async (req: Request, res: Response, next :NextFunction) => {
     const newUser: users = await usersModel.create(req.body);
-    res.status(201).json({ message: "User created successfully" });
+    res.status(201).json({ message: "User created successfully"});
 });
 
-export const getUsers = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+export const getUsers = asyncHandler(async (req: Request, res: Response, next :NextFunction) => {
     const allUsers = await usersModel.find();
-    res.status(200).json({ data: allUsers });
+    res.status(200).json({ data : allUsers});
 });
 
-export const getUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const user = await usersModel.findById(req.params.id);
-    res.status(200).json({ data: user });
+export const getUser = asyncHandler(async (req: Request, res: Response, next :NextFunction) => {
+    const  user = await usersModel.findById(req.params.id);
+    res.status(200).json({ data : user});
 });
 
-export const updateUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const user = await usersModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.status(200).json({ data: user });
+export const updateUser = asyncHandler(async (req: Request, res: Response, next :NextFunction) => {
+    const  user = await usersModel.findByIdAndUpdate(req.params.id,req.body,{new:true});
+    res.status(200).json({ data : user});
 });
 
-export const deleteUser = asyncHandler(async (req: Request, res: Response, next: NextFunction): Promise<any> => {
-    const user = await usersModel.findById(req.params.id);
-
+export const deleteUser = asyncHandler(async (req: Request, res: Response, next :NextFunction): Promise <any> => {
+    const  user = await usersModel.findById(req.params.id);
+   
     if (user!.fingerId) {
         const fingerprintDeleted = await deleteFingerprint(user!.fingerId);
-
+        
         if (fingerprintDeleted) {
-            const user = await usersModel.findByIdAndDelete(req.params.id);
+           const user = await usersModel.findByIdAndDelete(req.params.id);
             return res.status(500).json({ message: "User and his finger id deleted" });
-        } else {
-            res.status(200).json({ message: "Can't delete user" });
+        }else{
+            res.status(200).json({ massege : "Can't delete user"});
             console.log(user!.fingerId);
         }
     }
-
+    
 });
