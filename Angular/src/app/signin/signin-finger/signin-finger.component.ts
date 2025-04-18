8122bb2811d8443c554fb658d244aca098ee4b9a
Yasseen Ahmed <yasseenlmaghraby3@gmail.com>
2025-04-18 17:22:03 +0200

    small enhancements

--- Angular/src/app/signin/signin-finger/signin-finger.component.ts
+++ Angular/src/app/signin/signin-finger/signin-finger.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import {  CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'; 
+
 
 @Component({
   selector: 'app-signin-finger',
   imports: [],
   templateUrl: './signin-finger.component.html',
-  styleUrl: './signin-finger.component.scss'
+  styleUrl: './signin-finger.component.scss',
+    schemas: [CUSTOM_ELEMENTS_SCHEMA] ,
+  
 })
 export class SigninFingerComponent {
 
