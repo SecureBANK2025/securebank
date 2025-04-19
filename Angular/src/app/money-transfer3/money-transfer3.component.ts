20f8a631d20fe3aade37503edfba9852b269ca6a
Yasseen Ahmed <yasseenlmaghraby3@gmail.com>
2025-04-19 02:29:05 +0200

    transfer 3

--- Angular/src/app/money-transfer3/money-transfer3.component.ts
+++ Angular/src/app/money-transfer3/money-transfer3.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {  CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'; 
+
 
 @Component({
   selector: 'app-money-transfer3',
   imports: [],
   templateUrl: './money-transfer3.component.html',
-  styleUrl: './money-transfer3.component.scss'
+  styleUrl: './money-transfer3.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] ,
+
 })
 export class MoneyTransfer3Component {
 
