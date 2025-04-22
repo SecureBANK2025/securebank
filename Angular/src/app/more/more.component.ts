00519d0f83387f5d08da45ccf7a3975184b77926
Yasseen Ahmed <yasseenlmaghraby3@gmail.com>
2025-04-23 01:18:14 +0200

    More Page + some edits

--- Angular/src/app/more/more.component.ts
+++ Angular/src/app/more/more.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-more',
@@ -7,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrl: './more.component.scss'
 })
 export class MoreComponent {
+  constructor(
+    private _AuthService: AuthService,
+    private _router: Router) { }
+    ngOnInit(): void {
+      this._AuthService.checkToken();
+    }
 
+    navigateTo(route: string) {
+      this._router.navigate([`/${route}`]);
+      console.log(route);
+      console.log(this._router);
+
+    }
+    
+    logout() {
+      this._AuthService.logout();
+    }
+    back() {
+      this._router.navigate(['/mainOptions']);
+    }
 }
