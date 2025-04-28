04d79b25e01ae441b88fcb0191b09065a4d44c69
Yasseen Ahmed <yasseenlmaghraby3@gmail.com>
2025-04-28 20:25:03 +0300

    transactions history page

--- Angular/src/app/transactions-history/transactions-history.component.ts
+++ Angular/src/app/transactions-history/transactions-history.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-transactions-history',
-  imports: [],
+  selector: 'app-account-details',
+  imports: [CommonModule],
   templateUrl: './transactions-history.component.html',
   styleUrl: './transactions-history.component.scss'
 })
-export class TransactionsHistoryComponent {
+export class TransactionsHistoryComponent implements OnInit {
+  userData: any;
 
+  constructor(private _AuthService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this._AuthService.currentUser.subscribe(user => {
+      this.userData = user;
+    });
+  }
+
+  logout() {
+    this._AuthService.logout();
+  }
+
+  back() {
+    this.router.navigate(['/mainOptions']);
+  }
 }
