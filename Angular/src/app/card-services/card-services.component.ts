3e2aa935ffc0f26f91fa4afce5f63f1baefb0280
Baher Hossam <Baher1088@gmail.com>
2025-05-04 19:55:06 +0300

    freeze, unfreeze and avtivate disablement

--- Angular/src/app/card-services/card-services.component.ts
+++ Angular/src/app/card-services/card-services.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import { Location } from '@angular/common';
+// import { Location } from '@angular/common';
 import { DataService } from '../services/data.service';
+import { CardService } from '../services/card.service';
 
 
 @Component({
@@ -12,15 +13,42 @@ import { DataService } from '../services/data.service';
   templateUrl: './card-services.component.html',
   styleUrl: './card-services.component.scss'
 })
+
+
+
 export class CardComponent {
+
+  
+
+
+  activated: boolean = false;
+  freezed: boolean = false;
+  cardData: any;
+
+
   constructor(
     private _AuthService: AuthService,
-    private _router: Router ,
-    private _DataService: DataService
+    private _router: Router,
+    private _DataService: DataService,
+    private _CardService: CardService
   ) { }
 
   ngOnInit(): void {
     this._AuthService.checkToken();
+    this._CardService.getCardData().subscribe({
+      next: (res) => {
+        this.cardData = res.data[0];
+        console.log(this.cardData);
+        this.activated = this.cardData.isActive;
+        console.log(this.activated);
+        this.freezed = this.cardData.isFrozen;
+        console.log(this.freezed);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+    // this._DataService.refreshAllData();
   }
 
   logout() {
